3bd033a00993b30b9e269cf470cda42a4c348512
Steven Rodríguez Morales <91530422+StevenRguez@users.noreply.github.com>
2024-11-07 12:06:49 +0000

    Sillones añadidos

--- muebles-api/src/main/java/com/example/muebles_api/MuebleController.java
+++ muebles-api/src/main/java/com/example/muebles_api/MuebleController.java
@@ -24,7 +24,12 @@ public class MuebleController {
         ));
 
         mueblesPorCategoria.put("sofás", Arrays.asList(
-                new Mueble("1", "Sofá EKTORP", 299.99, "moderno", "sofá", "https://example.com/images/sofa_ektorp.jpg")
+                new Mueble("1", "Sofá LANDSKRONA", 499, "clasico", "sofá", "https://www.ikea.com/es/es/images/products/landskrona-sofa-2-plazas-gunnared-verde-claro-madera__0602106_pe680175_s5.jpg?f=xl")
+                new Mueble("1", "Sofá VINLIDEN", 499, "clasico", "sofá", "https://www.ikea.com/es/es/images/products/vinliden-sofa-2-plazas-hakebo-beige__0852697_pe780205_s5.jpg?f=xl")
+                new Mueble("1", "Sofá ESKILSTUNA", 799, "moderno", "sofá", "https://www.ikea.com/es/es/images/products/eskilstuna-sofa-3-plazas-con-chaiselongue-hillared-antracita__1256233_pe924958_s5.jpg?f=xl")
+                new Mueble("1", "Sofá BÅRSLÖV", 499, "moderno", "sofá", "https://www.ikea.com/es/es/images/products/barslov-sofa-cama-3-plazas-con-chaiselongue-tibbleby-beis-gris__1213693_pe911220_s5.jpg?f=xl")
+                new Mueble("1", "Sofá REVSKÄR", 596, "industrial", "sofá", "https://www.ikea.com/es/es/images/products/revskar-conjunto-3-plazas-exterior-antracita-froson-duvholmen-gris-oscuro__1240165_pe919197_s5.jpg?f=xl")
+                new Mueble("1", "Sofá KLIPPAN", 279, "industrial", "sofá", "https://www.ikea.com/es/es/images/products/klippan-sofa-2-plazas-vissle-gris__0239990_pe379591_s5.jpg?f=xl")
         ));
 
         mueblesPorCategoria.put("mesas", Arrays.asList(
