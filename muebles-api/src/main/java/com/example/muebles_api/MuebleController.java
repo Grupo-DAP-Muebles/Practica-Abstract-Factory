03d4e629116813550256a38fd42a8cd8907dc8b3
Steven Rodríguez Morales <91530422+StevenRguez@users.noreply.github.com>
2024-11-10 20:40:34 +0000

    Modificando IDs

--- muebles-api/src/main/java/com/example/muebles_api/MuebleController.java
+++ muebles-api/src/main/java/com/example/muebles_api/MuebleController.java
@@ -18,26 +18,26 @@ public class MuebleController {
         Map<String, List<Mueble>> mueblesPorCategoria = new HashMap<>();
 
         mueblesPorCategoria.put("sillas", Arrays.asList(
-                new Mueble("3", "Silla STEFAN", 35.00, "moderno", "silla", "https://www.ikea.com/es/es/images/products/stefan-silla-blanco__1322933_pe942332_s5.jpg?f=xl"),
-                new Mueble("4", "Silla JANINGE", 59.00, "moderno", "silla", "https://example.com/images/silla_janinge.jpg"),
-                new Mueble("5", "Silla ODFER", 79.00, "moderno", "silla", "https://example.com/images/silla_odfer.jpg"),
-                new Mueble("6", "Silla SKANSNAS", 89.00, "clasico", "silla", "https://www.ikea.com/es/es/images/products/skansnas-silla-marron-haya__1319860_pe941001_s5.jpg"),
-                new Mueble("7", "Silla NORRARYD", 89.00, "clasico", "silla", "https://www.ikea.com/es/es/images/products/norraryd-silla-negro__0727327_pe735599_s5.jpg"),
-                new Mueble("8", "Silla KULLABERG", 99.00, "industrial", "silla", "https://www.ikea.com/es/es/images/products/kullaberg-silla-confidente-pino-negro__0724693_pe734575_s5.jpg?f=xl")
+                new Mueble("1", "Silla STEFAN", 35.00, "moderno", "silla", "https://www.ikea.com/es/es/images/products/stefan-silla-blanco__1322933_pe942332_s5.jpg?f=xl"),
+                new Mueble("2", "Silla JANINGE", 59.00, "moderno", "silla", "https://example.com/images/silla_janinge.jpg"),
+                new Mueble("3", "Silla ODFER", 79.00, "moderno", "silla", "https://example.com/images/silla_odfer.jpg"),
+                new Mueble("4", "Silla SKANSNAS", 89.00, "clasico", "silla", "https://www.ikea.com/es/es/images/products/skansnas-silla-marron-haya__1319860_pe941001_s5.jpg"),
+                new Mueble("5", "Silla NORRARYD", 89.00, "clasico", "silla", "https://www.ikea.com/es/es/images/products/norraryd-silla-negro__0727327_pe735599_s5.jpg"),
+                new Mueble("6", "Silla KULLABERG", 99.00, "industrial", "silla", "https://www.ikea.com/es/es/images/products/kullaberg-silla-confidente-pino-negro__0724693_pe734575_s5.jpg?f=xl")
             
         ));
 
         mueblesPorCategoria.put("sofás", Arrays.asList(
-                new Mueble("1", "Sofá LANDSKRONA", 499, "clasico", "sofá", "https://www.ikea.com/es/es/images/products/landskrona-sofa-2-plazas-gunnared-verde-claro-madera__0602106_pe680175_s5.jpg?f=xl")
-                new Mueble("1", "Sofá VINLIDEN", 499, "clasico", "sofá", "https://www.ikea.com/es/es/images/products/vinliden-sofa-2-plazas-hakebo-beige__0852697_pe780205_s5.jpg?f=xl")
-                new Mueble("1", "Sofá ESKILSTUNA", 799, "moderno", "sofá", "https://www.ikea.com/es/es/images/products/eskilstuna-sofa-3-plazas-con-chaiselongue-hillared-antracita__1256233_pe924958_s5.jpg?f=xl")
-                new Mueble("1", "Sofá BÅRSLÖV", 499, "moderno", "sofá", "https://www.ikea.com/es/es/images/products/barslov-sofa-cama-3-plazas-con-chaiselongue-tibbleby-beis-gris__1213693_pe911220_s5.jpg?f=xl")
-                new Mueble("1", "Sofá REVSKÄR", 596, "industrial", "sofá", "https://www.ikea.com/es/es/images/products/revskar-conjunto-3-plazas-exterior-antracita-froson-duvholmen-gris-oscuro__1240165_pe919197_s5.jpg?f=xl")
-                new Mueble("1", "Sofá KLIPPAN", 279, "industrial", "sofá", "https://www.ikea.com/es/es/images/products/klippan-sofa-2-plazas-vissle-gris__0239990_pe379591_s5.jpg?f=xl")
+                new Mueble("7", "Sofá LANDSKRONA", 499, "clasico", "sofá", "https://www.ikea.com/es/es/images/products/landskrona-sofa-2-plazas-gunnared-verde-claro-madera__0602106_pe680175_s5.jpg?f=xl")
+                new Mueble("8", "Sofá VINLIDEN", 499, "clasico", "sofá", "https://www.ikea.com/es/es/images/products/vinliden-sofa-2-plazas-hakebo-beige__0852697_pe780205_s5.jpg?f=xl")
+                new Mueble("9", "Sofá ESKILSTUNA", 799, "moderno", "sofá", "https://www.ikea.com/es/es/images/products/eskilstuna-sofa-3-plazas-con-chaiselongue-hillared-antracita__1256233_pe924958_s5.jpg?f=xl")
+                new Mueble("10", "Sofá BÅRSLÖV", 499, "moderno", "sofá", "https://www.ikea.com/es/es/images/products/barslov-sofa-cama-3-plazas-con-chaiselongue-tibbleby-beis-gris__1213693_pe911220_s5.jpg?f=xl")
+                new Mueble("11", "Sofá REVSKÄR", 596, "industrial", "sofá", "https://www.ikea.com/es/es/images/products/revskar-conjunto-3-plazas-exterior-antracita-froson-duvholmen-gris-oscuro__1240165_pe919197_s5.jpg?f=xl")
+                new Mueble("12", "Sofá KLIPPAN", 279, "industrial", "sofá", "https://www.ikea.com/es/es/images/products/klippan-sofa-2-plazas-vissle-gris__0239990_pe379591_s5.jpg?f=xl")
         ));
 
         mueblesPorCategoria.put("mesas", Arrays.asList(
-                new Mueble("2", "Mesa LACK", 49.99, "moderno", "mesa", "https://example.com/images/mesa_lack.jpg")
+                new Mueble("13", "Mesa LACK", 49.99, "moderno", "mesa", "https://example.com/images/mesa_lack.jpg")
         ));
 
         return mueblesPorCategoria;
